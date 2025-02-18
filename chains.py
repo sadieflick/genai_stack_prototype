3db3266d098b64487fae51da594035f599e12970
Nathaniel Stern <nathanielstern@Mac.lan>
2025-02-18 08:14:50 -1000

    simplify file for only ollama  model option

--- chains.py
+++ chains.py
@@ -1,15 +1,11 @@
-
-from langchain_openai import OpenAIEmbeddings
+# Model & Embeddings
 from langchain_ollama import OllamaEmbeddings
-from langchain_aws import BedrockEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
-
-from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
-from langchain_aws import ChatBedrock
 
+# Vector Store
 from langchain_community.vectorstores import Neo4jVector
 
+# UI Request Processing
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 
@@ -21,83 +17,31 @@ from langchain.prompts import (
 
 from typing import List, Any
 from utils import BaseLogger, extract_title_and_question
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-
-AWS_MODELS = (
-    "ai21.jamba-instruct-v1:0",
-    "amazon.titan",
-    "anthropic.claude",
-    "cohere.command",
-    "meta.llama",
-    "mistral.mi",
-)
 
 def load_embedding_model(embedding_model_name: str, logger=BaseLogger(), config={}):
-    if embedding_model_name == "ollama":
-        embeddings = OllamaEmbeddings(
-            base_url=config["ollama_base_url"], model="llama2"
-        )
-        dimension = 4096
-        logger.info("Embedding: Using Ollama")
-    elif embedding_model_name == "openai":
-        embeddings = OpenAIEmbeddings()
-        dimension = 1536
-        logger.info("Embedding: Using OpenAI")
-    elif embedding_model_name == "aws":
-        embeddings = BedrockEmbeddings()
-        dimension = 1536
-        logger.info("Embedding: Using AWS")
-    elif embedding_model_name == "google-genai-embedding-001":        
-        embeddings = GoogleGenerativeAIEmbeddings(
-            model="models/embedding-001"
-        )
-        dimension = 768
-        logger.info("Embedding: Using Google Generative AI Embeddings")
-    else:
-        embeddings = HuggingFaceEmbeddings(
-            model_name="all-MiniLM-L6-v2", cache_folder="/embedding_model"
-        )
-        dimension = 384
-        logger.info("Embedding: Using SentenceTransformer")
+
+    embeddings = OllamaEmbeddings(
+        base_url=config["ollama_base_url"], model="llama2"
+    )
+    dimension = 4096
+    logger.info("Embedding: Using Ollama")
+
     return embeddings, dimension
 
 
 def load_llm(llm_name: str, logger=BaseLogger(), config={}):
-    if llm_name in ["gpt-4", "gpt-4o", "gpt-4-turbo"]:
-        logger.info("LLM: Using GPT-4")
-        return ChatOpenAI(temperature=0, model_name=llm_name, streaming=True)
-    elif llm_name == "gpt-3.5":
-        logger.info("LLM: Using GPT-3.5")
-        return ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", streaming=True)
-    elif llm_name == "claudev2":
-        logger.info("LLM: ClaudeV2")
-        return ChatBedrock(
-            model_id="anthropic.claude-v2",
-            model_kwargs={"temperature": 0.0, "max_tokens_to_sample": 1024},
-            streaming=True,
-        )
-    elif llm_name.startswith(AWS_MODELS):
-        logger.info(f"LLM: {llm_name}")
-        return ChatBedrock(
-            model_id=llm_name,
-            model_kwargs={"temperature": 0.0, "max_tokens_to_sample": 1024},
-            streaming=True,
-        )
 
-    elif len(llm_name):
-        logger.info(f"LLM: Using Ollama: {llm_name}")
-        return ChatOllama(
-            temperature=0,
-            base_url=config["ollama_base_url"],
-            model=llm_name,
-            streaming=True,
-            # seed=2,
-            top_k=10,  # A higher value (100) will give more diverse answers, while a lower value (10) will be more conservative.
-            top_p=0.3,  # Higher value (0.95) will lead to more diverse text, while a lower value (0.5) will generate more focused text.
-            num_ctx=3072,  # Sets the size of the context window used to generate the next token.
-        )
-    logger.info("LLM: Using GPT-3.5")
-    return ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", streaming=True)
+    logger.info(f"LLM: Using Ollama: {llm_name}")
+    return ChatOllama(
+        temperature=0,
+        base_url=config["ollama_base_url"],
+        model=llm_name,
+        streaming=True,
+        # seed=2,
+        top_k=10,  # A higher value (100) will give more diverse answers, while a lower value (10) will be more conservative.
+        top_p=0.3,  # Higher value (0.95) will lead to more diverse text, while a lower value (0.5) will generate more focused text.
+        num_ctx=3072,  # Sets the size of the context window used to generate the next token.
+    )
 
 
 def configure_llm_only_chain(llm):
